163a8360f61f72c039245803f6ab6c8e87e1da02
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-16 02:34:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].flatten
+     programmer_hash[:dennis_ritchie][:languages].join
      
 
 end
